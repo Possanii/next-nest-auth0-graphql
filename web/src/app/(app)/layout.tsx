c1f3eaca19fdd34c482385deead8fb46fb2610dc
Possanii <gpossanii@outlook.com>
2024-08-29 15:34:28 -0300

    feat: logout & authentication flow

---
+++ web/src/app/(app)/layout.tsx
@@ -0,0 +1,16 @@
+import { getSession } from '@auth0/nextjs-auth0'
+import { redirect } from 'next/navigation'
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const session = await getSession()
+
+  if (!session) {
+    redirect('/api/auth/login')
+  }
+
+  return <>{children}</>
+}
