92b18a0fd4d3c975a8ad1b87cd09fc1c7599e0cb
Possanii <gpossanii@outlook.com>
2024-08-29 16:00:10 -0300

    feat: prisma connection with db

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from 'src/auth/auth.module';
+import { DatabaseModule } from 'src/database/database.module';
 import { TestController } from './test.controller';
 
 @Module({
-  imports: [ConfigModule, AuthModule],
-  providers: [AuthModule],
+  imports: [ConfigModule, AuthModule, DatabaseModule],
+  providers: [],
   controllers: [TestController],
 })
 export class HttpModule {}
