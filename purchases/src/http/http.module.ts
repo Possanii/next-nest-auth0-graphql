5e4387606b746cb133b1c9b23d86a65b5d2b344c
Possanii <gpossanii@outlook.com>
2024-08-30 11:17:43 -0300

    feat: configure graphql

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -1,12 +1,22 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from 'src/auth/auth.module';
-import { DatabaseModule } from 'src/database/database.module';
-import { TestController } from './test.controller';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'node:path';
+import { AuthModule } from '../auth/auth.module';
+import { DatabaseModule } from '../database/database.module';
+import { TestResolver } from './test.resolver';
 
 @Module({
-  imports: [ConfigModule, AuthModule, DatabaseModule],
-  providers: [],
-  controllers: [TestController],
+  imports: [
+    ConfigModule,
+    AuthModule,
+    DatabaseModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      driver: ApolloDriver,
+    }),
+  ],
+  providers: [TestResolver],
 })
 export class HttpModule {}
