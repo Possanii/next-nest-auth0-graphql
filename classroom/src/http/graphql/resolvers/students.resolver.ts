479d42e985ae8c5e704541bb2aed70e9330ce834
Possanii <gpossanii@outlook.com>
2024-08-31 15:24:04 -0300

    feat: students resolve field enrollments

--- classroom/src/http/graphql/resolvers/students.resolver.ts
+++ classroom/src/http/graphql/resolvers/students.resolver.ts
@@ -1,16 +1,26 @@
 import { AuthorizationGuard } from '@/auth/authorization/authorization.guard';
+import { EnrollmentsService } from '@/services/enrollments.service';
 import { StudentsService } from '@/services/students.service';
 import { UseGuards } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Query, ResolveField, Resolver, Root } from '@nestjs/graphql';
+import { Enrollment } from '../models/enrollment';
 import { Student } from '../models/student';
 
 @Resolver(() => Student)
 export class StudentsResolver {
-  constructor(private studentsService: StudentsService) {}
+  constructor(
+    private studentsService: StudentsService,
+    private enrollmentsService: EnrollmentsService,
+  ) {}
 
   @Query(() => [Student])
   @UseGuards(AuthorizationGuard)
   students() {
     return this.studentsService.listAllStudents();
+  }
+
+  @ResolveField(() => Enrollment)
+  enrollments(@Root() student: Student) {
+    return this.enrollmentsService.listEnrollmentsByStudentId(student.id);
   }
 }
