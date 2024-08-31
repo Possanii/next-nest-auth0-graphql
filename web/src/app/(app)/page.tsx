82b5a5e03aaf13b7c477f024605b43b2b9e9d241
Possanii <gpossanii@outlook.com>
2024-08-31 13:04:21 -0300

    feat: logout when token expired

--- web/src/app/(app)/page.tsx
+++ web/src/app/(app)/page.tsx
@@ -1,7 +1,25 @@
+import { getAccessToken } from '@auth0/nextjs-auth0'
+import { redirect } from 'next/navigation'
+
 export default async function Dashboard() {
+  let accessToken: string | undefined
+
+  try {
+    const { accessToken: token } = await getAccessToken()
+    accessToken = token
+  } catch {
+    redirect('/api/auth/logout')
+  }
+
+  if (!accessToken) {
+    throw new Error('Invalid access token')
+  }
+
   return (
     <div>
       <h1>Dashboard</h1>
+
+      <pre>{accessToken}</pre>
 
       <a href="/api/auth/logout">Logout</a>
     </div>
